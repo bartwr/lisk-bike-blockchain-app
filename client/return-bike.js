021a93a53c9c888266129c86f1e8a1c41a34c99d
Eric van Riet Paap <ericvrp@gmail.com>
2019-10-21 11:44:02 +0200

    Testcode for return-bike. Add returnbike custom transaction

---
+++ client/return-bike.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+const fs = require('fs');
+const { APIClient } = require('@liskhq/lisk-client');
+const ReturnBikeTransaction = require('../transactions/return-bike');
+
+const { getTimestamp, getBike } = require('./_helpers.js');
+
+const client = new APIClient([`http://${process.env.HTTP_HOST}:${process.env.HTTP_PORT}`]);
+const renterAccount = JSON.parse(fs.readFileSync('./accounts/'+process.argv[2]+'.json')); 
+const bikeAccount = JSON.parse(fs.readFileSync('./accounts/'+process.argv[3]+'.json')); 
+
+
+const returnBike = (bike, renterAccount) => {
+
+    const tx =  new ReturnBikeTransaction({
+        asset: {
+            id: bike.id, // XXX or use bike.address
+        },
+        amount: bike.deposit,
+        senderPublicKey: renterAccount.publicKey,
+        recipientId: bike.id,
+        timestamp: getTimestamp(),
+    });
+    // console.log(tx);
+
+    tx.sign(renterAccount.passphrase);
+
+    return client.transactions.broadcast(tx.toJSON())
+    .then(() => tx)
+    .catch(err => {
+      console.error("err2:", err);
+      // return Promise.reject(err);
+    });
+  }
+
+// test-coode
+getBike(client, bikeAccount).then(bike => {
+  console.log("bike:", bike);
+
+  returnBike(bike, renterAccount).then(rentResult => {
+    console.log(rentResult);
+  });
+})
+
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const CreateBikeTransaction = require('./transactions/create-bike'); // require 
 const FaucetTransaction = require('./transactions/faucet'); // require the newly created transaction type 'HelloTransaction'
 const UpdateBikeLocationTransaction = require('./transactions/update-bike-location'); // require the newly created transaction type 'HelloTransaction'
 const RentBikeTransaction = require('./transactions/rent-bike'); // require the newly created transaction type 'HelloTransaction'
+const ReturnBikeTransaction = require('./transactions/return-bike');
 
 configDevnet.app.label = 'lisk-bike-blockchain-app'; // change the label of the app
 configDevnet.components.storage.password = 'lisk123'; // replace password with the password for your database user
@@ -16,6 +17,7 @@ app.registerTransaction(CreateBikeTransaction);
 app.registerTransaction(FaucetTransaction);
 app.registerTransaction(UpdateBikeLocationTransaction);
 app.registerTransaction(RentBikeTransaction);
+app.registerTransaction(ReturnBikeTransaction);
 
 // the code block below starts the application and doesn't need to be changed
 app
