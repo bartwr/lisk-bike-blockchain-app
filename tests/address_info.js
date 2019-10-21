80f75f72572ffdf8699562a510d487d1e83089c5
Eric van Riet Paap <ericvrp@gmail.com>
2019-10-21 15:26:19 +0200

    Query code to get the balance of an address

---
+++ tests/address_info.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const fs = require('fs');
+const { exec } = require('child_process');
+
+const account = JSON.parse(fs.readFileSync('./accounts/'+process.argv[2]+'.json')); 
+const url = `http://${process.env.HTTP_HOST}:${process.env.HTTP_PORT}/api/accounts?address=${account.address}`;
+// console.log(url);
+
+exec(`curl ${url}`, (err, stdout, stderr) => {
+	if (err) {
+		//some err occurred
+		console.error(err)
+	} else {
+		// the *entire* stdout and stderr (buffered)
+		console.log(JSON.parse(stdout));
+		//console.log(`stdout: ${stdout}`);
+		//console.log(`stderr: ${stderr}`);
+	}
+});
+
